4b7ce43e3ba6c105b85ef6daf1e3447fb1dd962a
mateusz055 <mateusziskra591@gmail.com>
2022-01-08 11:26:34 +0100

    kick off

--- WURefineBot/MainWindow.xaml.cs
+++ WURefineBot/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace WpfApp4
+namespace WURefineBot
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
