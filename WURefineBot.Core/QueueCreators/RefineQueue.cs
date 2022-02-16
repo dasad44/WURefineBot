8062cb45b3e6eb64b419982e71114a165b4a7c05
mateusz055 <mateusziskra591@gmail.com>
2022-02-16 14:23:04 +0100

    Resources

--- WURefineBot.Core/QueueCreators/RefineQueue.cs
+++ WURefineBot.Core/QueueCreators/RefineQueue.cs
@@ -13,10 +13,16 @@ namespace WURefineBot.Core.QueueCreators
     class RefineQueue
     {
         public List<Bitmap> CreateQueue(string pictures)
-        {         
+        {       
             List<Bitmap> ImageList = new List<Bitmap>();
             ScreenHandler screenHandler = new ScreenHandler();
-            screenHandler.GetImage(pictures);               
+            screenHandler.GetImage(pictures);
+            ImageList.Add(WURefineBot.Core.Properties.Resources.Menu);
+            ImageList.Add(WURefineBot.Core.Properties.Resources.equipment);
+            ImageList.Add(WURefineBot.Core.Properties.Resources.resources);
+            ImageList.Add(WURefineBot.Core.Properties.Resources.lasers);
+            //to do : wybór surowca
+            ImageList.Add(WURefineBot.Core.Properties.Resources.enrich);
             return new List<Bitmap>(CreateQueue(pictures));
         }      
     }
