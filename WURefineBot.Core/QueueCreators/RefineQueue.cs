e1864759cc17e1fdbf436dfdd9b7b3be6ba59e87
mateusz055 <mateusziskra591@gmail.com>
2022-02-16 14:11:42 +0100

    images

--- WURefineBot.Core/QueueCreators/RefineQueue.cs
+++ WURefineBot.Core/QueueCreators/RefineQueue.cs
@@ -1,20 +1,22 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using WURefineBot.Infrastructure.Imaging;
+using WURefineBot;
 
 namespace WURefineBot.Core.QueueCreators
 {
     class RefineQueue
     {
         public List<Bitmap> CreateQueue(string pictures)
-        {
-            
+        {         
+            List<Bitmap> ImageList = new List<Bitmap>();
             ScreenHandler screenHandler = new ScreenHandler();
-            screenHandler.GetImage(pictures);
+            screenHandler.GetImage(pictures);               
             return new List<Bitmap>(CreateQueue(pictures));
         }      
     }
