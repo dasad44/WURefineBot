53815d80b6dd72484db1621281769c791f70b424
mateusz055 <mateusziskra591@gmail.com>
2022-04-02 10:51:37 +0200

    service

---
+++ WURefineBot.Core/Interfaces/Services/IRefineService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WURefineBot.Core.Interfaces.Services
+{
+    public interface IRefineService
+    {
+        void ExecuteRefine(WURefineBot.Core.Enums.Resources resource);
+    }
+}
--- WURefineBot.Core/Services/RefineService.cs
+++ WURefineBot.Core/Services/RefineService.cs
@@ -3,10 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WURefineBot.Core.Interfaces.QueueCreators;
+using WURefineBot.Core.Interfaces.Services;
 
 namespace WURefineBot.Core.Services
 {
-    class RefineService
+    public class RefineService : IRefineService
     {
+        private readonly IRefineQueue _refineQueue ;
+        public RefineService(IRefineQueue refineQueue)
+        {
+            _refineQueue = refineQueue;
+        }
+        public void ExecuteRefine(WURefineBot.Core.Enums.Resources resource)
+        {
+            _refineQueue.GetQueue(resource);
+        }
     }
 }
